b7984d894d63f5acf6e7d5f0b75e3ed751713f9b
Stefan <47391841+ostef@users.noreply.github.com>
2025-11-27 16:42:05 +0100

    Update source.c

--- level8/source.c
+++ level8/source.c
@@ -5,16 +5,18 @@
 #include <string.h>
 
 char *auth;
-void *service;
+char *service;
 
 int main() {
     char s[136];
 
     while (true) {
         printf("%p, %p \n", auth, service);
+
         if (!fgets(s, 128, stdin)) {
             break;
         }
+
         if (!memcmp(s, "auth ", 5)) {
             auth = (char *)malloc(4);
             memset(auth, 0, 4);
@@ -24,13 +26,16 @@ int main() {
                 strcpy(auth, name);
             }
         }
+
         if (!memcmp(s, "reset", 5)) {
             free(auth);
         }
+
         if (!memcmp(s, "service", 6)) {
             char *name = s + 7;
             service = strdup(name);
         }
+
         if (!memcmp(s, "login", 5)) {
             if (*((uint32_t *)auth + 8)) {
                 system("/bin/sh");
